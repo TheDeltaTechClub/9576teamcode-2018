262835358e0bf9e6309a4f414f198c9a3023a57c
foliagecanine <45408062+foliagecanine@users.noreply.github.com>
2018-11-27 15:34:09 -0800

    Add files via upload

---
+++ basicTeleop.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="basicTeleop")
+public class basicTeleop extends OpMode {
+
+        DcMotor leftDrive;
+        DcMotor rightDrive;
+
+        public void basicTeleop() {
+
+        }
+
+        @Override
+        public void init() {
+            leftDrive = hardwareMap.dcMotor.get("leftMotor");
+            rightDrive = hardwareMap.dcMotor.get("rightMotor");
+            leftDrive.setDirection(DcMotor.Direction.FORWARD);
+            rightDrive.setDirection(DcMotor.Direction.REVERSE);
+            telemetry.addData("Init finished.", "");
+            telemetry.update();
+        }
+
+        @Override
+        public void loop() {
+            float left = gamepad1.left_stick_y;
+            float right = gamepad1.right_stick_y;
+
+            left = Range.clip(left, -1, 1);
+            right = Range.clip(right, -1, 1);
+
+            leftDrive.setPower(left);
+            rightDrive.setPower(right);
+        }
+
+        @Override
+        public void stop() {
+
+        }
+}
